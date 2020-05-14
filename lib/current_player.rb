117d9bbef55fa27d57763718f1d2fb44bd7b4aa4
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-14 16:28:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,10 @@
 #turn_count method takes in an argument of the board array and returns the number of turns that have been played
-def turn_count
-
+def turn_count(board)
+  if       /2 % == 0
+     return "X"
+   else
+     return "O"
+   end
 end
 
 #current_player method should take in an argument of the game board and use the #turn_count method to determine if it is "X"'s turn or "O"'s
