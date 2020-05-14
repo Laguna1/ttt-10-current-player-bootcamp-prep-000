3541e7ab6d647243137eb1cf1afb8cf6cfa801e5
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-14 16:22:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,4 +3,8 @@ def turn_count
 
 end
 
-#current_player 
+#current_player method should take in an argument of the game board and use the #turn_count method to determine if it is "X"'s turn or "O"'s
+
+def current_player
+
+end
